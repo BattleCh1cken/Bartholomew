8ce00193585214bc82c1b00fae1f08af2cf97bd5
BattleCh1cken <BattleCh1cken@larkov.de>
2023-10-16 17:50:43 -0400

    :sparkles: Wrote out database schema

---
+++ migrations/20231014184958_init.sql
@@ -0,0 +1,28 @@
+create table if not exists teams
+(
+  id integer primary key autoincrement not null,
+  name string not null,
+  leader bigint not null,
+  guild bigint, 
+  foreign key(guild) references guilds(id)
+);
+
+create table if not exists guilds 
+(
+  id bigint primary key not null,
+  name string not null
+);
+
+create table if not exists users
+(
+  id bigint primary key not null
+);
+
+create table if not exists scores 
+(
+  id integer primary key autoincrement not null,
+  user bigint not null, 
+  team bigint not null,
+  foreign key(user) references users(id),
+  foreign key(team) references teams(id)
+);
